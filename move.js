002501044d492bd77123f2d0e569c02b4dacf5d2
Valiamaximova1 <120101026+valentinamaximova@users.noreply.github.com>
2023-01-19 13:52:58 +0200

    today

--- move.js
+++ move.js
@@ -19,7 +19,7 @@ let layout = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1],
+    [1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
@@ -33,6 +33,14 @@ var ypos = 200;
 var xspeed = 1;
 var yspeed = 0;
 var maxSpeed = 1;
+var xposE = 121;
+var yposE = 200;
+var xspeedE = 1;
+var yspeedE = 0;
+var maxSpeedE = 1;
+// let emenyBrains = [
+//     red, yellow, green, pink
+// ]
 
 var upPressed = 0;
 var downPressed = 0;
@@ -41,6 +49,24 @@ var rightPressed = 0;
 
 let maxWidth = 224;
 let maxHeight = 247;
+
+// function moveEnemy() {
+//     let enemy = document.getElementById('item');
+
+//     xposE = xposE + xspeedE;
+//     yposE = yposE + yspeedE;
+
+//     enemy.style.left = xposE;
+//     enemy.style.top = yposE;
+
+//     yspeedE = Math.floor(Math.random() * 20);
+//     xspeedE = Math.floor(Math.random() * 20);
+
+//     // if (upPressed == 0 && downPressed == 0)
+//     //     slowDownY();
+//     // if (leftPressed == 0 && rightPressed == 0)
+//     //     slowDownX();
+// }
 
 function slowDownX() {
     if (xspeed > 0)
@@ -56,20 +82,56 @@ function slowDownY() {
         yspeed = yspeed + 1;
 }
 
+// function enemyMove() {
+//     xposE = xposE + xspeedE;
+//     yposE = yposE + yspeedE;
+
+//     enemy = document.getElementById('item')
+//     enemy.style.left = xposE;
+//     enemy.style.top = yposE;
+//     // console.log(xposE);
+//     // console.log(yposE);
+//     if (xposE === maxHeight || ypos === maxWidth) {
+
+//     } else {
+//         if (upPressed == 1)
+//             yspeedE = Math.max(yspeedE - 1, -1 * maxSpeedE);
+//         if (downPressed == 1)
+//             yspeedE = Math.min(yspeedE + 1, 1 * maxSpeedE)
+//         if (rightPressed == 1)
+//             xspeedE = Math.min(xspeedE + 1, 1 * maxSpeedE);
+//         if (leftPressed == 1)
+//             xspeedE = Math.max(xspeedE - 1, -1 * maxSpeedE);
+
+//         if (upPressed == 0 && downPressed == 0)
+//             slowDownY();
+//         if (leftPressed == 0 && rightPressed == 0)
+//             slowDownX();
+//     }
+
+//     setTimeout("enemyMove()", 10);
+// }
+
 
 function gameLoop() {
     xpos = xpos + xspeed;
     ypos = ypos + yspeed;
 
     pacman = document.getElementById('pacman')
+
+    if (xpos >= maxWidth || ypos >= maxHeight || ypos <= 0 || xpos <= 0) {
+        xpos = xpos - 10;
+        ypos = ypos - 10;
+        console.log(maxHeight);
+        console.log(maxWidth);
+        console.log("xpos = " + xpos);
+        console.log("ypos = " + ypos);
+
+    } else {
+
+    }
     pacman.style.left = xpos;
     pacman.style.top = ypos;
-
-
-    // for (let i = 0; i < layout.length; i++) {
-    //     for (let j = 0; j < layout.length; j++) {
-    //         if (layout[i][j] == 0) {
-    // console.log(layout[i][j]);
     if (upPressed == 1)
         yspeed = Math.max(yspeed - 1, -1 * maxSpeed);
     if (downPressed == 1)
@@ -83,15 +145,7 @@ function gameLoop() {
         slowDownY();
     if (leftPressed == 0 && rightPressed == 0)
         slowDownX();
-    // }
-    //     }
 
-
-    // }
-
-
-
-    // loop
     setTimeout("gameLoop()", 10);
 }
 
@@ -120,6 +174,7 @@ function keyUp(e) {
         rightPressed = 0;
 }
 document.addEventListener("DOMContentLoaded", gameLoop)
-document.addEventListener("load", gameLoop)
+    // document.addEventListener("DOMContentLoaded", enemyMove)
+
 document.addEventListener("keydown", keyDown)
 document.addEventListener("keyup", keyUp)
