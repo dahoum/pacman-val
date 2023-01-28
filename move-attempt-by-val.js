e94194ee532266335add19eb47854b878bc05c31
T. E. Shaw <t.e.shaw@dahoum.wales>
2023-01-28 13:25:57 +0100

    renamed move back to the last working version

---
+++ move-attempt-by-val.js
@@ -0,0 +1,150 @@
+let layout = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 3, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 10, 10, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6],
+]
+document.addEventListener("keydown", keyDown)
+var xpos = 0;
+var ypos = 0;
+var xspeed = 1;
+var yspeed = 0;
+var maxSpeed = 1;
+
+const MovingDirection = {
+    up: 0,
+    down: 1,
+    left: 2,
+    right: 3,
+}
+
+var upPressed = 0;
+var downPressed = 0;
+var leftPressed = 0;
+var rightPressed = 0;
+
+let maxWidth = 224;
+let maxHeight = 247;
+
+let currentPosition = 0;
+let curentMovingDirection = null;
+let requectMovingDirection = null;
+pacman = document.getElementById('pacman')
+
+function getPacman() {
+    for (let row = 0; row < layout.length; row++) {
+        for (let column = 0; column < layout[row].length; column++) {
+            let tile = layout[row][column];
+            // console.log(tile);
+            console.log(column);
+            console.log(row);
+            if (tile === 5) {
+                xpos = column
+                ypos = row
+                pacman.style.left = xpos;
+                pacman.style.top = ypos;
+
+            }
+        }
+    }
+    console.log(xpos);
+    console.log(ypos);
+}
+
+function keyDown(e) {
+    // up
+    if (e.keyCode == 38) {
+        upPressed = 1;
+        if (curentMovingDirection == MovingDirection.down) {
+            this.curentMovingDirection = MovingDirection.up
+        }
+        this.requectMovingDirection = MovingDirection.up
+
+    }
+
+    // down
+    if (e.keyCode == 40) {
+        downPressed = 1;
+        if (curentMovingDirection == MovingDirection.up) {
+            this.curentMovingDirection = MovingDirection.down
+        }
+        this.requectMovingDirection = MovingDirection.down
+    }
+
+    // left
+    if (e.keyCode == 37) {
+        leftPressed = 1;
+        if (curentMovingDirection == MovingDirection.right) {
+            this.curentMovingDirection = MovingDirection.left
+        }
+        this.requectMovingDirection = MovingDirection.left
+    }
+
+    // right
+    if (e.keyCode == 39) {
+        rightPressed = 1;
+        if (curentMovingDirection == MovingDirection.right) {
+            this.curentMovingDirection = MovingDirection.left
+        }
+        this.requectMovingDirection = MovingDirection.left
+    }
+
+}
+
+function move() {
+
+    // console.log(xpos);
+    // console.log(ypos);
+    if (curentMovingDirection !== this.requectMovingDirection) {
+        if (Number.isInteger(this.x / 8.82) && Number.isInteger(this.y / 8)) {
+            this.curentMovingDirection = this.requectMovingDirection
+        }
+    }
+
+    switch (this.curentMovingDirection) {
+        case MovingDirection.up:
+            this.ypos -= maxSpeed
+            break
+        case MovingDirection.down:
+            this.ypos += maxSpeed
+            break
+        case MovingDirection.left:
+            this.xpos -= maxSpeed
+            break
+        case MovingDirection.right:
+            this.xpos += maxSpeed
+            break
+
+    }
+}
+
+document.addEventListener("DOMContentLoaded", getPacman)
+    // document.addEventListener("DOMContentLoaded", enemyMove)
+
+
+// setInterval(getPacman, 1000 / 75);
+// setInterval(move, 1000 / 75);
