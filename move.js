41bf0f1c647598acee50b12a4643a3b8522de3b7
Valiamaximova1 <120101026+valentinamaximova@users.noreply.github.com>
2023-01-28 14:04:26 +0200

    new

--- move.js
+++ move.js
@@ -19,7 +19,7 @@ let layout = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1],
+    [1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
     [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
@@ -28,19 +28,19 @@ let layout = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6],
 ]
-var xpos = 121;
-var ypos = 200;
+document.addEventListener("keydown", keyDown)
+var xpos = 0;
+var ypos = 0;
 var xspeed = 1;
 var yspeed = 0;
 var maxSpeed = 1;
-var xposE = 121;
-var yposE = 200;
-var xspeedE = 1;
-var yspeedE = 0;
-var maxSpeedE = 1;
-// let emenyBrains = [
-//     red, yellow, green, pink
-// ]
+
+const MovingDirection = {
+    up: 0,
+    down: 1,
+    left: 2,
+    right: 3,
+}
 
 var upPressed = 0;
 var downPressed = 0;
@@ -50,131 +50,101 @@ var rightPressed = 0;
 let maxWidth = 224;
 let maxHeight = 247;
 
-// function moveEnemy() {
-//     let enemy = document.getElementById('item');
+let currentPosition = 0;
+let curentMovingDirection = null;
+let requectMovingDirection = null;
+pacman = document.getElementById('pacman')
 
-//     xposE = xposE + xspeedE;
-//     yposE = yposE + yspeedE;
+function getPacman() {
+    for (let row = 0; row < layout.length; row++) {
+        for (let column = 0; column < layout[row].length; column++) {
+            let tile = layout[row][column];
+            // console.log(tile);
+            console.log(column);
+            console.log(row);
+            if (tile === 5) {
+                xpos = column
+                ypos = row
+                pacman.style.left = xpos;
+                pacman.style.top = ypos;
 
-//     enemy.style.left = xposE;
-//     enemy.style.top = yposE;
-
-//     yspeedE = Math.floor(Math.random() * 20);
-//     xspeedE = Math.floor(Math.random() * 20);
-
-//     // if (upPressed == 0 && downPressed == 0)
-//     //     slowDownY();
-//     // if (leftPressed == 0 && rightPressed == 0)
-//     //     slowDownX();
-// }
-
-function slowDownX() {
-    if (xspeed > 0)
-        xspeed = xspeed - 1;
-    if (xspeed < 0)
-        xspeed = xspeed + 1;
-}
-
-function slowDownY() {
-    if (yspeed > 0)
-        yspeed = yspeed - 1;
-    if (yspeed < 0)
-        yspeed = yspeed + 1;
-}
-
-// function enemyMove() {
-//     xposE = xposE + xspeedE;
-//     yposE = yposE + yspeedE;
-
-//     enemy = document.getElementById('item')
-//     enemy.style.left = xposE;
-//     enemy.style.top = yposE;
-//     // console.log(xposE);
-//     // console.log(yposE);
-//     if (xposE === maxHeight || ypos === maxWidth) {
-
-//     } else {
-//         if (upPressed == 1)
-//             yspeedE = Math.max(yspeedE - 1, -1 * maxSpeedE);
-//         if (downPressed == 1)
-//             yspeedE = Math.min(yspeedE + 1, 1 * maxSpeedE)
-//         if (rightPressed == 1)
-//             xspeedE = Math.min(xspeedE + 1, 1 * maxSpeedE);
-//         if (leftPressed == 1)
-//             xspeedE = Math.max(xspeedE - 1, -1 * maxSpeedE);
-
-//         if (upPressed == 0 && downPressed == 0)
-//             slowDownY();
-//         if (leftPressed == 0 && rightPressed == 0)
-//             slowDownX();
-//     }
-
-//     setTimeout("enemyMove()", 10);
-// }
-
-
-function gameLoop() {
-    xpos = xpos + xspeed;
-    ypos = ypos + yspeed;
-
-    pacman = document.getElementById('pacman')
-
-    if (xpos >= maxWidth || ypos >= maxHeight || ypos <= 0 || xpos <= 0) {
-        xpos = xpos - 10;
-        ypos = ypos - 10;
-        console.log(maxHeight);
-        console.log(maxWidth);
-        console.log("xpos = " + xpos);
-        console.log("ypos = " + ypos);
-
-    } else {
-
+            }
+        }
     }
-    pacman.style.left = xpos;
-    pacman.style.top = ypos;
-    if (upPressed == 1)
-        yspeed = Math.max(yspeed - 1, -1 * maxSpeed);
-    if (downPressed == 1)
-        yspeed = Math.min(yspeed + 1, 1 * maxSpeed)
-    if (rightPressed == 1)
-        xspeed = Math.min(xspeed + 1, 1 * maxSpeed);
-    if (leftPressed == 1)
-        xspeed = Math.max(xspeed - 1, -1 * maxSpeed);
-
-    if (upPressed == 0 && downPressed == 0)
-        slowDownY();
-    if (leftPressed == 0 && rightPressed == 0)
-        slowDownX();
-
-    setTimeout("gameLoop()", 10);
+    console.log(xpos);
+    console.log(ypos);
 }
 
 function keyDown(e) {
-    let code = e.keyCode ? e.keyCode : e.which;
-    if (code == 38)
+    // up
+    if (e.keyCode == 38) {
         upPressed = 1;
-    console.log("da");
-    if (code == 40)
+        if (curentMovingDirection == MovingDirection.down) {
+            this.curentMovingDirection = MovingDirection.up
+        }
+        this.requectMovingDirection = MovingDirection.up
+
+    }
+
+    // down
+    if (e.keyCode == 40) {
         downPressed = 1;
-    if (code == 37)
+        if (curentMovingDirection == MovingDirection.up) {
+            this.curentMovingDirection = MovingDirection.down
+        }
+        this.requectMovingDirection = MovingDirection.down
+    }
+
+    // left
+    if (e.keyCode == 37) {
         leftPressed = 1;
-    if (code == 39)
+        if (curentMovingDirection == MovingDirection.right) {
+            this.curentMovingDirection = MovingDirection.left
+        }
+        this.requectMovingDirection = MovingDirection.left
+    }
+
+    // right
+    if (e.keyCode == 39) {
         rightPressed = 1;
+        if (curentMovingDirection == MovingDirection.right) {
+            this.curentMovingDirection = MovingDirection.left
+        }
+        this.requectMovingDirection = MovingDirection.left
+    }
+
 }
 
-function keyUp(e) {
-    let code = e.keyCode ? e.keyCode : e.which;
-    if (code == 38)
-        upPressed = 0;
-    if (code == 40)
-        downPressed = 0;
-    if (code == 37)
-        leftPressed = 0;
-    if (code == 39)
-        rightPressed = 0;
+function move() {
+
+    // console.log(xpos);
+    // console.log(ypos);
+    if (curentMovingDirection !== this.requectMovingDirection) {
+        if (Number.isInteger(this.x / 8.82) && Number.isInteger(this.y / 8)) {
+            this.curentMovingDirection = this.requectMovingDirection
+        }
+    }
+
+    switch (this.curentMovingDirection) {
+        case MovingDirection.up:
+            this.ypos -= maxSpeed
+            break
+        case MovingDirection.down:
+            this.ypos += maxSpeed
+            break
+        case MovingDirection.left:
+            this.xpos -= maxSpeed
+            break
+        case MovingDirection.right:
+            this.xpos += maxSpeed
+            break
+
+    }
 }
-document.addEventListener("DOMContentLoaded", gameLoop)
+
+document.addEventListener("DOMContentLoaded", getPacman)
     // document.addEventListener("DOMContentLoaded", enemyMove)
 
-document.addEventListener("keydown", keyDown)
-document.addEventListener("keyup", keyUp)
+
+// setInterval(getPacman, 1000 / 75);
+// setInterval(move, 1000 / 75);
